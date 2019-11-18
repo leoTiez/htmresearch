8e6b67029da6c885e66c8f9de3c4357361b9c65a
Jakob Heyder <kaktus.w.h@gmx.de>
2019-11-18 16:47:38 +0100

    Adding a minimal experiment test (no printouts/figures) and moving the apical test

--- tests/bayesian/test_apical_tiebreak_bayesian_temporal_memory.py
+++ tests/bayesian/test_apical_tiebreak_bayesian_temporal_memory.py
@@ -3,10 +3,7 @@ import unittest
 import numpy as np
 
 # Import apical tiebreak bayesian tm
-import sys
-import os
-sys.path.append(os.path.realpath(os.path.join(os.getcwd(), '../algorithms')))
-import apical_tiebreak_bayesian_temporal_memory as btm
+import htmresearch.algorithms.apical_tiebreak_bayesian_temporal_memory as btm
 
 
 class BayesianTMTest(unittest.TestCase):
@@ -191,4 +188,3 @@ class BayesianTMTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
