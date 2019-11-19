46cb926c4fd5a336e66b52ac649be016097fd367
Leo Tiez <leozeitler95@gmail.com>
2019-11-19 23:49:08 +0100

    Update: Implemented global setup and runs experiment script

--- run-experiments.sh
+++ run-experiments.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+pip install .
+echo "Updated environment. Run experiments"
+echo "####################################"
+python2.7 tests/bayesian/bayesian_convergence_activity_minimal.py
