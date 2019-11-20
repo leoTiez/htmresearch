70c62f69724db5712cfd0e6df28b03eeb5f491bd
Leo Tiez <leozeitler95@gmail.com>
2019-11-20 10:59:44 +0100

    Update: Renaming to deploy-n-test

--- run-experiments.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pip install .
-echo "Updated environment. Run experiments"
-echo "####################################"
-python2.7 tests/bayesian/bayesian_convergence_activity_minimal.py
